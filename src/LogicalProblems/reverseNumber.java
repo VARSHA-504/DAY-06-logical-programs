743fa57a8076491e8b07ef20a0323af87901513b
varsha-504 <antargangivarsha.com>
2022-03-13 14:22:45 +0530

    Added : to print reverseNumber

---
+++ src/LogicalProblems/reverseNumber.java
@@ -0,0 +1,26 @@
+package LogicalProblems;
+
+import java.util.Scanner;
+
+public class reverseNumber {
+    public static void main(String[] args)
+    {
+        int number, reverse = 0;
+
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter Number to be Reversed");
+        number=sc.nextInt();
+
+        while(number != 0)
+        {
+            int remainder = number % 10;
+            reverse = reverse * 10 + remainder;
+            number = number/10;
+        }
+
+        System.out.println("The reverse of the given number is: " + reverse);
+        sc.close();
+    }
+
+}
+
